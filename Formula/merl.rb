528599e311caef6a794d4f580e92fc6b3fb84246
Maksim Burtsev <zadrot-lol@list.ru>
2026-09-25 08:09:20 +0400

    merl 0.7.0

--- Formula/merl.rb
+++ Formula/merl.rb
@@ -1,24 +1,24 @@
 class Merl < Formula
   desc "Keyboard-only code navigator for the terminal"
   homepage "https://github.com/Maksim-Burtsev/merl"
-  version "0.6.0"
+  version "0.7.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/Maksim-Burtsev/merl/releases/download/v0.6.0/merl-aarch64-apple-darwin.tar.gz"
-      sha256 "4055445108d5ef4336336ee02b55dd656ff30a3b77e4adcc5a3687c60f940788"
+      url "https://github.com/Maksim-Burtsev/merl/releases/download/v0.7.0/merl-aarch64-apple-darwin.tar.gz"
+      sha256 "ead3e46bfb9097ec7eaff34a6d5192af17b2a1d048857235a242c5039ce6586a"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/Maksim-Burtsev/merl/releases/download/v0.6.0/merl-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "0f96628e9e5b596e93534a54c0bfa963f91c540a6166fffc7123f80b375f0b47"
+      url "https://github.com/Maksim-Burtsev/merl/releases/download/v0.7.0/merl-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "d2f305961b58da946428b4fcca97875aad35c5cd2c1c5537a93e39c30b7187d3"
     end
     on_arm do
-      url "https://github.com/Maksim-Burtsev/merl/releases/download/v0.6.0/merl-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "b3ab180637db2f1746117cdc113c47c835448a0380a1ac2fac2208d0c04c8859"
+      url "https://github.com/Maksim-Burtsev/merl/releases/download/v0.7.0/merl-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "22863e496d3ea2f34b9332eb11d2313cc3db67559e24f50dd34885607e61ca67"
     end
   end
 
@@ -27,6 +27,6 @@ class Merl < Formula
   end
 
   test do
-    assert_match "merl 0.6.0", shell_output("#{bin}/merl --version")
+    assert_match "merl 0.7.0", shell_output("#{bin}/merl --version")
   end
 end
